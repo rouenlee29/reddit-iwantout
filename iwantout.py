5f0df9d99fb4c07bae472f586b29246897a6954d
Rou En Lee <jeanleerouen@gmail.com>
2020-05-01 21:37:03 +0800

    may 1st work

--- iwantout.py
+++ iwantout.py
@@ -66,6 +66,16 @@ def most_frequent(List):
     occurence_count = Counter(List) 
     return occurence_count.most_common(1)[0][0] 
 
+def check_continent_words(String):
+        is_continent_word = False
+        continent_words = ["south", "north", "east", "west", "central"]
+
+        for c in continent_words:
+            if c in String:
+                is_continent_word = True
+                break
+        return is_continent_word
+
 def find_country_using_fuzzy(List):
 
     """
@@ -75,6 +85,8 @@ def find_country_using_fuzzy(List):
     search_fuzzy will guess 'zealand' to be 'new zealand'
     we take the most common guess, i.e 'new zealand'
     """
+    # words that will belong to a continent and not a country
+    
 
     country_name = None
     candidates = []
@@ -94,6 +106,10 @@ def find_country_using_fuzzy(List):
         elif len(s) == 1:
             # one letter words are unlikely to be countries 
             # but search_fuzzy will try to guess countries from it 
+            pass
+
+        # check if `s` contains any words in continent_words
+        elif check_continent_words(s) == True:
             pass
 
         else: 
@@ -135,7 +151,7 @@ def extract_title(title):
     job_and_origin_str = job_and_countries.split("->")[0].strip()
     job_and_origin_list = job_and_origin_str.split(" ") 
     origin_country = find_country_using_fuzzy(job_and_origin_list)
-    origin_region = get_destination_region([origin_country], job_and_origin_str)
+    origin_region = map_country_to_continent(origin_country)
 
     job = find_job(job_and_origin_list)
 
@@ -190,31 +206,44 @@ def map_country_to_continent(String):
         continent_name = None
     return continent_name
     
-    
+def capitalise_every_word(String):
+    """
+    "south america" becomes "South America"
+    """
 
-def get_destination_region(dest_countries_list, raw_dest_str):
+    List = String.split(" ")
+    capitalised_list = []
+
+    for L in List:
+        capitalised_list.append(L.capitalize())
+
+    capitalised_str = " ".join(capitalised_list)
+
+    return capitalised_str
+
+def get_destination_region(raw_string, country_list):
     
     """
     `region` can mean continent (e.g Asia, Europe) or subregion (European Union, Scandinavia)
-    """
+    String contains ONLY name of a country 
+    """    
     dest_region = []
     
-    # see if any continents are contained in raw string 
-    region = ["africa","antarctica","asia","europe","north america","south america","oceania", "scandinavia"]
-    dest_region += [c if c in raw_dest_str else None for c in region]
-    
-    if ("eu," in raw_dest_str) or (",eu" in raw_dest_str):
-        dest_region += ['european union']
-    
-    # map countries to continens 
-    if dest_countries_list != []:
-        dest_region += [map_country_to_continent(d) for d in dest_countries_list]
+    if country_list == []: # no countries found 
+        # find manually from raw string
+        region = ["africa","antarctica","asia","europe","north america","south america","oceania", "scandinavia"]
+        dest_region += [capitalise_every_word(c) if c in raw_string else None for c in region]
+
+        if ("eu," in raw_string) or (",eu" in raw_string):
+            dest_region += ['European Union']
+
+        dest_region = list(set(dest_region)) # remove duplicates
         
-    # take unique values
-    dest_region = list(set(dest_region))
-    
-    if None in dest_region:
-        dest_region.remove(None)
+        if dest_region != [None]:
+            dest_region.remove(None)
+    else:
+        for c in country_list:
+            dest_region.append(map_country_to_continent(c))
     
     return dest_region
 
@@ -223,26 +252,76 @@ def get_destination(String):
     """
     Assumes countries are separated by symbols or text
     e.g. "us/uk", "australia or zimbabwe"
+
+    returns a list of countries identified from fuzzy search 
     """
     
     String = String.lower()
     String = re.sub(r"( or |\\|/| and |\|)", ',', String)
-    
-    country_list = String.split(",")
-    dest = []
+    List = String.split(",")
+
+    dest_country_list = []
     region = []
     
     #print(country_list)
-    for c in country_list:
+    for c in List:
         
         if ("?" in c) or ("anywhere" in c) or (" x " in c):
-            dest.append("anywhere")
+            dest_country_list.append("anywhere")
 
         else:
-            fuzzy_result = find_country_using_fuzzy([c])
+            fuzzy_result = find_country_using_fuzzy([c.strip()])
             if fuzzy_result is not None:
-                dest.append(fuzzy_result)
-    #print(String)
-    dest_region = get_destination_region(dest, String)
+                dest_country_list.append(fuzzy_result)
+    
+    dest_region_list = get_destination_region(String, dest_country_list)
+
+    if dest_country_list == []:
+        dest_country_list = [None] * len(dest_region_list)
             
-    return dest, dest_region
+    return dest_country_list, dest_region_list
+
+
+# expldoe function below is...
+# taken from https://stackoverflow.com/questions/45846765/efficient-way-to-unnest-explode-multiple-list-columns-in-a-pandas-dataframe
+def explode(df, lst_cols, fill_value=''):
+    # make sure `lst_cols` is a list
+    if lst_cols and not isinstance(lst_cols, list):
+        lst_cols = [lst_cols]
+    # all columns except `lst_cols`
+    idx_cols = df.columns.difference(lst_cols)
+
+    # calculate lengths of lists
+    lens = df[lst_cols[0]].str.len()
+
+    if (lens > 0).all():
+        # ALL lists in cells aren't empty
+        return pd.DataFrame({
+            col:np.repeat(df[col].values, df[lst_cols[0]].str.len())
+            for col in idx_cols
+        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
+          .loc[:, df.columns]
+    else:
+        # at least one list in cells is empty
+        return pd.DataFrame({
+            col:np.repeat(df[col].values, df[lst_cols[0]].str.len())
+            for col in idx_cols
+        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
+          .append(df.loc[lens==0, idx_cols]).fillna(fill_value) \
+          .loc[:, df.columns]
+
+# example of `explode` function usage:
+# df
+# 	A	        B           C
+# 0	[1, 2, 3]	[2, 3, 4]   1
+# 1	[3,4]	    [4, 5]      2
+# 2	[5]	        [6]         3
+
+# explode(df,['A','B'])
+# 	A	B	C
+# 0	1	2	1
+# 1	2	3	1
+# 2	3	4	1
+# 3	3	4	2
+# 4	4	5	2
+# 5	5	6	3
